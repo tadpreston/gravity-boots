c43353ce34eb6ba57b5829a9caeafc207af8796d
Tad Preston <tadpreston@gmail.com>
2018-04-30 09:18:16 -0500

    sucessful basic deploy

--- Capfile
+++ Capfile
@@ -33,7 +33,8 @@ require "capistrano/bundler"
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
 require 'capistrano/puma'
-# require "capistrano/passenger"
+install_plugin Capistrano::Puma
+install_plugin Capistrano::Puma::Nginx
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,9 @@ set :deploy_to, "/var/www/assetpanda"
 set :rbenv_type
 set :rbenv_ruby, '2.5.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all
+
 
 append :linked_dirs, '.bundle'
 
@@ -29,7 +30,7 @@ append :linked_dirs, '.bundle'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", '.rbenv-vars'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
